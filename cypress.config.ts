c07a40b180e6ef58ce751ce1207178361f1867c4
Kaic <kaic.melo@totvs.com.br>
2023-06-06 18:33:31 -0300

    feat: adicionando code coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "cypress";
+import task from "@cypress/code-coverage/task";
+// import istanbul from "@cypress/code-coverage/use-browserify-istanbul";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      task(on, config);
+      return config;
+      // on("task", task);
+      // on("file:preprocessor", istanbul);
     },
   },
 });
